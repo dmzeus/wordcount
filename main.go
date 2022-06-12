0081a05ada42b8bfcfcf11ce5dfc5923fd447259
Dmitry Mikolaets <Dmitry.Mikolaets@kaspersky.com>
2022-06-13 00:29:49 +0300

    ref

--- main.go
+++ main.go
@@ -6,14 +6,9 @@ import (
 	"strings"
 )
 
-func WordCount(str string) int {
-	length := len(strings.Fields(str))
-	return length
-}
-
 func main() {
-	str := os.Args[1]
-	length := WordCount(str)
+	inputString := os.Args[1]
+	length := len(strings.Fields(inputString))
 
-	fmt.Printf("word count in string '%v' is %v.\n", str, length)
+	fmt.Printf("word count in string '%v' is %v.\n", inputString, length)
 }
